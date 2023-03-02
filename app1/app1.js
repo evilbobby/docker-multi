9a34c1f259c415bf3ba861016e8d550b2ae40dac
EvilBobby <bobby.borszich@gmail.com>
2023-03-01 17:16:20 -0700

    2apps and sql working, not sharing db

--- app1/app1.js
+++ app1/app1.js
@@ -1,14 +1,46 @@
-console.log("starting app 1")
+const appName = "App1"
+const port = 3000
+
+console.log(`starting ${appName}`)
 
 const express = require('express')
 const app = express()
-const port = 3000
-
+const db = require('./persistence');
+const { v4: uuid } = require('uuid');
+ 
 app.get('/', (req, res) => {
-  res.send('Hello World! App1')
+
   console.log("got request");
+
+  const item = {
+    id: uuid(),
+    name: `todo item ${appName}`,
+    completed: false,
+  };
+
+  (async () => await db.storeItem(item))();
+
+  db.getItems().then(rows =>
+    console.log("rows: " + rows.length)
+  );
+
+  res.send(`Hello World! ${appName}`)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+db.init().then(() => {
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+
+const gracefulShutdown = () => {
+  db.teardown()
+    .catch(() => { })
+    .then(() => process.exit());
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
